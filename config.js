e79370736e8d1eb3c8ce7a9d4845aeea1c1d69d4
ssMinji <ssong758@gmail.com>
2024-02-05 14:38:05 +0900

    fix time

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const KAKAOTALK_API_TOKEN = "e7d33583af296c6b09678b7906974b11";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
-export const WEDDING_DATE = "2024년 02월 24일, 토요일 오후 3시";
+export const WEDDING_DATE = "2024년 02월 24일, 토요일 오후 3시 20분";
 export const WEDDING_LOCATION = "더컨벤션 잠실, 3층 아모르홀";
 
 export const GROOM_NAME = "김태호";
